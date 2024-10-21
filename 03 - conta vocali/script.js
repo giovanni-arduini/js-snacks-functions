d57f809af8333018030ea57d55e80163d7fa9eb9
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-21 16:33:26 +0200

    ADD solution to conta vocali

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,24 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+/* Scrivi una funzione che accetti una stringa e 
+restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
 
+function howManyVowelsIn(parola) {
+  const vowels = "AaIiUuEeOo";
+  let wordVowels = [];
+
+  for (i = 0; i < parola.length; i++) {
+    letter = parola.charAt(i);
+    if (vowels.includes(letter)) {
+      wordVowels.push(letter);
+    }
+  }
+  return wordVowels.length;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
-
+console.log("Il numero di vocali nella parola è", howManyVowelsIn(word));
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
